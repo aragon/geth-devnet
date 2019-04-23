171784b71e9f0460dd7f725e3c9735ec4e5cfd0c
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-04-23 10:52:58 -0300

    Polish dev node starting script (#4)
    * Polish dev node script
    
    * Add funding accounts statement for dev chain starting script

--- run-dev-node.js
+++ run-dev-node.js
@@ -1,16 +1,12 @@
-function createAccounts() {
-    for (var i = 0; i < 10; i++) {
-        console.log('creating new account...')
-        acc = personal.newAccount("");
-        personal.unlockAccount(acc, "");
-        eth.sendTransaction({from: eth.accounts[0], to: acc, value: web3.toWei(1000, "ether")});
-    }
-}
-
-function unlockAccounts() {
+function unlockAndFundAccounts() {
+  firstAccount = eth.accounts[0]
   eth.accounts.forEach(function (account) {
     console.log('Unlocking ' + account + '...');
     personal.unlockAccount(account, '', 86400);
+    if (account !== firstAccount) {
+      console.log('Sending 10K ETH to ' + account + '...')
+      eth.sendTransaction({ from: firstAccount, to: account, value: web3.toWei(10000, "ether") });
+    }
   });
 }
 
@@ -27,10 +23,19 @@ function pendingTransactions() {
 }
 
 function setupDevNode() {
-  // keep accounts unlocked
-  while (true) {
-      unlockAccounts()
-  }
+  web3.eth.filter("pending").watch(function () {
+    if (miner.hashrate > 0) return;
+    console.log("== Pending transactions! Looking for next block...");
+    miner.start(8);
+  });
+
+  web3.eth.filter("latest").watch(function () {
+    if (!pendingTransactions()) {
+      console.log("== No transactions left. Stopping miner...");
+      miner.stop();
+    }
+  });
 }
 
+unlockAndFundAccounts();
 setupDevNode();
